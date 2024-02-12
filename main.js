e67fbae7fa81399b83b8d6da4c64faf0b8f79019
azizbek400 <aziznormurodov400@gmail.com>
2024-02-13 02:06:10 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//   1   ////////////////////////////////////////
+
+// const me = {
+//     firstName: "Azizbek",
+//     lastName: "Nurmurodov",
+//     age: 17,
+//     languages: ["js", "python", "c++", "nodejs"],
+//     friends: ["Dilshod", "sunnat", "Aziz", "Ali"],
+// };
+
+// console.log(me);
